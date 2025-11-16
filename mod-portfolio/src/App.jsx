85c09a0d95985313a1b53659a09edd3625f43315
Jorwin Reyes <jorwin2002@gmail.com>
2025-11-16 17:30:31 -0500

    navbar integration

--- mod-portfolio/src/App.jsx
+++ mod-portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from './sections/Navbar.jsx';
 import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Projects from './sections/Projects.jsx';
@@ -9,6 +10,7 @@ import Footer from './sections/Footer.jsx';
 const App = () => {
     return (
         <main className='max-w-7xl mx-auto relative'>
+            <Navbar />
             <Hero />
             <About />
             <Projects />
